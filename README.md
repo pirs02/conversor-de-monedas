1341d86017b2c16047fb843c3bdfaba83a0173b0
Andres Altamirano <153695119+pirs02@users.noreply.github.com>
2024-10-20 15:11:24 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Conversor de Monedas.
+Challenge correspondiente al curso Java Orientado a Objetos G7 – ONE de la especialización de Back-End.
+
+## Descripción:
+Este es un proyecto realizado en Java en el cual consumiremos la Api de Exchange Rate API que nos permitirá realizar la conversión entres diferentes divisas, esta aplicación tambien genera un historial de las consultas realizadas y en este historial también veremos la fecha y hora de las consultas realizadas.
+
+## Tecnologías Utilizadas:
+
+- **Lenguaje de Programación:** Java
+- **API de Tasas de Cambio:** Exchange Rate API.
+- **Biblioteca Gson:** Gson se empleó para analizar la respuesta JSON de la API y convertirla en objetos Java para su manipulación.
+- **Control de Versiones:** Git/GitHub se usaron para el control de versiones del proyecto y la colaboración en equipo.
+- **Entorno de Desarrollo Integrado (IDE):** IntelliJ IDEA fue el entorno de desarrollo utilizado para escribir, depurar y ejecutar el código Java.
+
+## Desarrollado por:
+- Andrés de Jesús Altamirano Pinos
---
+++ src/ConsultaConversion.java
@@ -0,0 +1,36 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+// Es responsable de realizar las consultas a una API para obtener las tasas de cambio entre diferentes monedas.
+public class ConsultaConversion {
+    public String buscaConversion(String monedaBase, String monedaObjetivo, double cantidad) {
+        try {
+            URI direccion = URI.create("https://v6.exchangerate-api.com/v6/d7e1c897e5e26ef3e454bb66/pair/" +
+                    monedaBase + "/" + monedaObjetivo + "/" + cantidad);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(direccion)
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            var json = response.body();
+            JsonElement jsonElement = JsonParser.parseString(json);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+            return jsonObject.get("conversion_result").getAsString();
+
+        } catch (NumberFormatException | IOException | InterruptedException e) {
+            System.out.println("Ocurrió un error: ");
+            throw new RuntimeException("Error" + e.getMessage());
+        }
+    }
+}
